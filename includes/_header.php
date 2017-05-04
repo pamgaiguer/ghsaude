7f654cc3987a71c7475c250c454fe81aeb63b2ee
Pam Gaiguer <pamella.gaiguer@gmail.com>
2017-05-04 00:35:41 -0300

    topo ok

---
+++ includes/_header.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="author" content="Pamella Gaiguer">
+  <meta name="Description" content="">
+  <link rel="stylesheet" href="css/custom.css">
+  <title>GH Saude</title>
+</head>
+<body>
+
+  <header>
+    <nav class="navbar navbar-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-top" aria-expanded="false">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+        </div>
+
+        <div class="collapse navbar-collapse" id="navbar-top">
+          <ul class="nav navbar-nav navbar-right">
+            <li><a href="#">Institucional</a></li>
+            <li><a href="#">Produtos</a></li>
+            <li><a href="#">Rede credenciada</a></li>
+            <li><a href="#">Beneficiário</a></li>
+            <li><a href="#">Credenciado</a></li>
+            <li><a href="#">Cliente Pessoa Jurídica</a></li>
+            <li><a href="#">Notícias</a></li>
+          </ul>
+        </div>
+
+      </div>
+    </nav>
+    <nav class="navbar nav-submenu">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#nav-submenu" aria-expanded="false">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#"><img src="images/logos/logo_gh.png" alt=""></a>
+        </div>
+
+        <div class="collapse navbar-collapse" id="nav-submenu">
+          <ul class="nav navbar-nav navbar-right">
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Solicite uma visita</a></li>
+            <li><a href="#">Ouvidoria</a></li>
+            <li><a href="#">Fale conosco</a></li>
+          </ul>
+        </div>
+
+      </div>
+
+    </nav>
+
+  </header>
--- index.php
+++ index.php
@@ -1,48 +1,12 @@
-<!DOCTYPE html>
-<html lang="pt">
-<head>
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <meta name="author" content="Pamella Gaiguer">
-  <meta name="Description" content="">
-  <link rel="stylesheet" href="css/custom.css">
-  <title>GH Saude</title>
-</head>
-<body>
+<?php include 'includes/_header.php';?>
 
-  <header>
-    <nav class="navbar navbar-top">
-      <div class="container">
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-top" aria-expanded="false">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-        </div>
+<main class="home">
 
-        <div class="collapse navbar-collapse" id="navbar-top">
-          <ul class="nav navbar-nav navbar-right">
-                <li><a href="#">Institucional</a></li>
-                <li><a href="#">Produtos</a></li>
-                <li><a href="#">Rede credenciada</a></li>
-                <li><a href="#">Beneficiário</a></li>
-                <li><a href="#">Credenciado</a></li>
-                <li><a href="#">Cliente Pessoa Jurídica</a></li>
-                <li><a href="#">Notícias</a></li>
-          </ul>
-        </div>
-
-      </div>
-    </nav>
-  </header>
-
-  <footer>
-  </footer>
-  <script src="js/jquery-3.2.1.min.js"></script>
-  <script src="js/bootstrap.min.js"></script>
-  <script src="js/main.js"></script>
+</main>
+<footer>
+</footer>
+<script src="js/jquery-3.2.1.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/main.js"></script>
 </body>
 </html>
