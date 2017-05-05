a085d71101d498fb186d2323ef6da3bba36831c3
Pam Gaiguer <pamella.gaiguer@gmail.com>
2017-05-04 23:42:00 -0300

    partial footer

--- index.php
+++ index.php
@@ -3,48 +3,5 @@
 <main class="home">
 
 </main>
-<footer class="footer">
-  <div class="purple-strip">
 
-    <div class="container">
-      <div class="row">
-        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-          <!-- <img src="images/" alt="" class="img-responsive center-block"> -->
-          <p>Av. Tancredo Neves, 620, sala 715 - Edf. Mundo Plaza - Caminho das Árvores - Salvador - Bahia - CEP: 41820-020</p>
-          <p class="tel-footer">Tel. + 55 71. 3039-4800/3032-1414</p>
-        </div>
-      </div>
-    </div>
-  </div>
-  <div class="container">
-    <div class="row">
-      <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-        <div class="text-center">
-          <img src="images/logos/logo_ans.png" alt="">
-          <img src="images/logos/logo_ans_cod.png" alt="">
-        </div>
-      </div>
-    </div>
-    <div class="row">
-      <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-        <ul class="list-unstyled list-inline list-ans">
-          <li class="text-uppercase">Disque ANS</li>
-          <li>0800 7019656</li>
-          <li><a href="http://www.ans.gov.br" target="_blank">http://www.ans.gov.br</a></li>
-        </ul>
-      </div>
-    </div>
-        <div class="row">
-      <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-      <p class="copyright">
-        Todos os direitos reservados À GH SAÚDE - Copyright 2017
-      </p>
-      </div>
-      </div>
-  </div>
-</footer>
-<script src="js/jquery-3.2.1.min.js"></script>
-<script src="js/bootstrap.min.js"></script>
-<script src="js/main.js"></script>
-</body>
-</html>
+<?php include 'includes/_footer.php';?>
