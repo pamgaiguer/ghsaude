188ea05157e190abafc08a6b2e031026cad25233
Pam Gaiguer <pamella.gaiguer@gmail.com>
2017-05-08 23:45:37 -0300

    home

--- index.php
+++ index.php
@@ -26,13 +26,13 @@
       <i class="material-icons">keyboard_arrow_right</i>
     </a>
   </div>
-<div class="spacing"></div>
+  <div class="spacing"></div>
   <div class="container">
     <div class="row">
       <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
         <div class="box-services">
           <div class="box-blue">
-          <img src="images/rede_cred.png" class="img-responsive center-block" alt="">
+            <img src="images/rede_cred.png" class="img-responsive center-block" alt="">
             <h2>Rede Credenciada</h2>
             <p>
               Aqui você poderá consultar toda a nossa Rede Credenciada e escolher
@@ -40,7 +40,7 @@
             </p>
           </div>
           <div class="box-yellow">
-          <img src="images/beneficiario-blue.png" class="img-responsive center-block" alt="">
+            <img src="images/beneficiario-blue.png" class="img-responsive center-block" alt="">
             <h2>Beneficiário</h2>
             <p>
               Acesso restrito para beneficiários do cartão GH Saúde.
@@ -48,7 +48,7 @@
             </p>
           </div>
           <div class="box-blue">
-          <img src="images/casinha.png" class="img-responsive center-block" alt="">
+            <img src="images/casinha.png" class="img-responsive center-block" alt="">
             <h2>Credenciado</h2>
             <p>
               Acesso restrito para os parceiros credenciados ao cartão GH Saúde.
@@ -56,7 +56,7 @@
             </p>
           </div>
           <div class="box-yellow">
-          <img src="images/pessoajuridica.png" class="img-responsive center-block" alt="">
+            <img src="images/pessoajuridica.png" class="img-responsive center-block" alt="">
             <h2>Cliente Pessoa Jurídica</h2>
             <p>
               Sua empresa é nossa cliente? Acesse aqui seus relatórios, extratos, segunda vida de boletos,
@@ -64,6 +64,48 @@
             </p>
           </div>
           <div class="clearfix"></div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+
+  <div class="container">
+    <div class="infos-partners">
+      <div class="row">
+        <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
+          <div class="grupp-halsa">
+            <img src="images/logos/logo_halsa.png" alt="">
+            <div class="spacing"></div>
+            <p>
+              O Grupp Hälsa trouxe para o Brasil uma tecnologia de gestão de medicina de grupo, espelhada na competência dos sistemas
+              de gestão internacional, onde a organização, capacitação continuada e logística operacional, são fatores de
+              excelência na velocidade e qualidade do atendimento, possibilitando proporcionar, além da segurança, a qualidade de vida que você e sua família sempre sonharam.
+            </p>
+            <p>
+              A GH SAÚDE, Unidade Brasil, é a operadora de medicina de grupo, que pertence ao Grupo Hälsa, com sede em Salvador - Bahia,
+              onde opera uma ampla rede credenciada de clínicas, laboratórios e hospitais, criada cuidadosamente para atender com eficiência as necessidades de seus associados.
+            </p>
+          </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
+          <div class="doubts-about">
+            <img src="images/logos/logo_halsa.png" alt="">
+            <div class="spacing"></div>
+            <p>
+              A GH SAÚDE possui um moderno Contract Center dotado da mais avançada tecnologia e técnicos qualificados que garantem o suporte necessário ao seu segurado.
+              24 horas por dia, todos os dias, o ano inteiro. Precisou?
+            </p>
+            <p class="blue-info">
+              É só ligar:
+            </p>
+            <p class="yellow-info">
+              0800-0004-1414
+            </p>
+            <p class="yellow-darker-info">
+              (71) 3039-4800 | (71) 3032-1414
+            </p>
+          </div>
         </div>
       </div>
     </div>
