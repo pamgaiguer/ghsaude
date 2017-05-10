eebd988da4786f928d27ba2319312b73bd2b289b
Pam Gaiguer <pamella.gaiguer@gmail.com>
2017-05-10 01:53:14 -0300

    pagina de noticias

---
+++ news.php
@@ -0,0 +1,110 @@
+<?php include 'includes/_header.php';?>
+
+<main class="news">
+  <div id="slider-home" class="carousel slide" data-ride="carousel">
+    <div class="carousel-inner" role="listbox">
+      <div class="item active">
+        <img src="images/slider/slider_1.jpg">
+      </div>
+      <div class="item">
+        <img src="images/slider/slider_2.jpg">
+      </div>
+      <div class="item">
+        <img src="images/slider/slider_3.jpg">
+      </div>
+      <div class="item">
+        <img src="images/slider/slider_4.jpg">
+      </div>
+    </div>
+
+    <a class="left carousel-control" href="#slider-home" role="button" data-slide="prev">
+      <i class="material-icons">keyboard_arrow_left</i>
+    </a>
+    <a class="right carousel-control" href="#slider-home" role="button" data-slide="next">
+      <i class="material-icons">keyboard_arrow_right</i>
+    </a>
+  </div>
+  <div class="spacing"></div>
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+        <div class="box-services hidden-xs">
+          <div class="box-blue">
+            <img src="images/rede_cred.png" class="img-responsive center-block" alt="">
+            <h2>Rede Credenciada</h2>
+            <p>
+              Aqui você poderá consultar toda a nossa Rede Credenciada e escolher
+              o melhor credenciado para cuidar de você e sua família.
+            </p>
+          </div>
+          <div class="box-yellow">
+            <img src="images/beneficiario-blue.png" class="img-responsive center-block" alt="">
+            <h2>Beneficiário</h2>
+            <p>
+              Acesso restrito para beneficiários do cartão GH Saúde.
+              Aqui você poderá consultar sua situação cadastral, agendamentos e outras informações.
+            </p>
+          </div>
+          <div class="box-blue">
+            <img src="images/casinha.png" class="img-responsive center-block" alt="">
+            <h2>Credenciado</h2>
+            <p>
+              Acesso restrito para os parceiros credenciados ao cartão GH Saúde.
+              Aqui você poderá efetuar consultas diversas, transmitir faturas, retirar guias e outros.
+            </p>
+          </div>
+          <div class="box-yellow">
+            <img src="images/pessoajuridica.png" class="img-responsive center-block" alt="">
+            <h2>Cliente Pessoa Jurídica</h2>
+            <p>
+              Sua empresa é nossa cliente? Acesse aqui seus relatórios, extratos, segunda vida de boletos,
+              recibos e outros.
+            </p>
+          </div>
+          <div class="clearfix"></div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+        <h3 class="title">
+          <img src="images/pgIcon_news.png" alt="">
+          Notícias
+        </h3>
+        <p>Confira abaixo as principais notícias da GH SAÚDE e também, da saúde no país.</p>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4">
+        <div class="thumbnail">
+          <img src="images/placeholders/img_ph_2.png" class="img-responsive" alt="...">
+          <div class="spacing"></div>
+          <div class="caption">
+            <h3>Titulo da notícia</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea eum repellendus repudiandae sit nulla iusto earum fugiat ipsam eaque laborum non commodi, provident odit laboriosam animi necessitatibus incidunt consequuntur praesentium!</p>
+
+            <div class="spacing"></div>
+            <div class="spacing"></div>
+
+            <p>
+              <a href="#">
+                <img src="images/mais_amarelo.png" alt="">
+                Leia mais
+              </a>
+            </p>
+            <div class="clearfix"></div>
+          </div>
+        </div>
+      </div>
+      <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4"></div>
+      <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4"></div>
+    </div>
+  </div>
+
+
+</main>
+
+<?php include 'includes/_footer.php';?>
