3bba82e73b430ee09d4f7baff662e401f9a57739
Pam Gaiguer <pamella.gaiguer@gmail.com>
2017-05-13 16:44:39 -0300

    news block done

--- news.php
+++ news.php
@@ -77,18 +77,24 @@
         <p>Confira abaixo as principais notícias da GH SAÚDE e também, da saúde no país.</p>
       </div>
     </div>
+
+    <div class="spacing"></div>
+    <div class="spacing"></div>
+    <div class="spacing"></div>
+
     <div class="row">
       <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4">
         <div class="thumbnail">
-          <img src="images/placeholders/img_ph_2.png" class="img-responsive" alt="...">
           <div class="spacing"></div>
+          <img src="images/placeholders/img_ph_2.png" class="img-responsive" alt="...">
+          <div class="news-date">
+            <p>20/02/2017</p>
+          </div>
+
           <div class="caption">
             <h3>Titulo da notícia</h3>
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea eum repellendus repudiandae sit nulla iusto earum fugiat ipsam eaque laborum non commodi, provident odit laboriosam animi necessitatibus incidunt consequuntur praesentium!</p>
-
             <div class="spacing"></div>
-            <div class="spacing"></div>
-
             <p>
               <a href="#">
                 <img src="images/mais_amarelo.png" alt="">
