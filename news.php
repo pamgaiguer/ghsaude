d9e44bd3eea9bf1293b0d2c087daca5066c4eaf9
Pam Gaiguer <pamella.gaiguer@gmail.com>
2017-05-13 18:45:35 -0300

    news page

--- news.php
+++ news.php
@@ -95,19 +95,191 @@
             <h3>Titulo da notícia</h3>
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea eum repellendus repudiandae sit nulla iusto earum fugiat ipsam eaque laborum non commodi, provident odit laboriosam animi necessitatibus incidunt consequuntur praesentium!</p>
             <div class="spacing"></div>
-            <p>
-              <a href="#">
-                <img src="images/mais_amarelo.png" alt="">
-                Leia mais
-              </a>
-            </p>
+            <div class="spacing"></div>
+            <a href="#" class="btn-plus plus-yellow">
+              Leia mais
+            </a>
             <div class="clearfix"></div>
           </div>
         </div>
       </div>
-      <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4"></div>
-      <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4"></div>
+
+      <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4">
+        <div class="thumbnail">
+          <div class="spacing"></div>
+          <img src="images/placeholders/img_ph_2.png" class="img-responsive" alt="...">
+          <div class="news-date">
+            <p>20/02/2017</p>
+          </div>
+
+          <div class="caption">
+            <h3>Titulo da notícia</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea eum repellendus repudiandae sit nulla iusto earum fugiat ipsam eaque laborum non commodi, provident odit laboriosam animi necessitatibus incidunt consequuntur praesentium!</p>
+            <div class="spacing"></div>
+            <div class="spacing"></div>
+            <a href="#" class="btn-plus plus-blue">
+              Leia mais
+            </a>
+            <div class="clearfix"></div>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4">
+        <div class="thumbnail">
+          <div class="spacing"></div>
+          <img src="images/placeholders/img_ph_2.png" class="img-responsive" alt="...">
+          <div class="news-date">
+            <p>20/02/2017</p>
+          </div>
+
+          <div class="caption">
+            <h3>Titulo da notícia</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea eum repellendus repudiandae sit nulla iusto earum fugiat ipsam eaque laborum non commodi, provident odit laboriosam animi necessitatibus incidunt consequuntur praesentium!</p>
+            <div class="spacing"></div>
+            <div class="spacing"></div>
+            <a href="#" class="btn-plus plus-yellow">
+              Leia mais
+            </a>
+            <div class="clearfix"></div>
+          </div>
+        </div>
+      </div>
+
     </div>
+
+    <div class="row">
+      <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4">
+        <div class="thumbnail">
+          <div class="spacing"></div>
+          <img src="images/placeholders/img_ph_2.png" class="img-responsive" alt="...">
+          <div class="news-date">
+            <p>20/02/2017</p>
+          </div>
+
+          <div class="caption">
+            <h3>Titulo da notícia</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea eum repellendus repudiandae sit nulla iusto earum fugiat ipsam eaque laborum non commodi, provident odit laboriosam animi necessitatibus incidunt consequuntur praesentium!</p>
+            <div class="spacing"></div>
+            <div class="spacing"></div>
+            <a href="#" class="btn-plus plus-yellow">
+              Leia mais
+            </a>
+            <div class="clearfix"></div>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4">
+        <div class="thumbnail">
+          <div class="spacing"></div>
+          <img src="images/placeholders/img_ph_2.png" class="img-responsive" alt="...">
+          <div class="news-date">
+            <p>20/02/2017</p>
+          </div>
+
+          <div class="caption">
+            <h3>Titulo da notícia</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea eum repellendus repudiandae sit nulla iusto earum fugiat ipsam eaque laborum non commodi, provident odit laboriosam animi necessitatibus incidunt consequuntur praesentium!</p>
+            <div class="spacing"></div>
+            <div class="spacing"></div>
+            <a href="#" class="btn-plus plus-blue">
+              Leia mais
+            </a>
+            <div class="clearfix"></div>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4">
+        <div class="thumbnail">
+          <div class="spacing"></div>
+          <img src="images/placeholders/img_ph_2.png" class="img-responsive" alt="...">
+          <div class="news-date">
+            <p>20/02/2017</p>
+          </div>
+
+          <div class="caption">
+            <h3>Titulo da notícia</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea eum repellendus repudiandae sit nulla iusto earum fugiat ipsam eaque laborum non commodi, provident odit laboriosam animi necessitatibus incidunt consequuntur praesentium!</p>
+            <div class="spacing"></div>
+            <div class="spacing"></div>
+            <a href="#" class="btn-plus plus-yellow">
+              Leia mais
+            </a>
+            <div class="clearfix"></div>
+          </div>
+        </div>
+      </div>
+
+    </div>
+
+    <div class="row">
+      <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4">
+        <div class="thumbnail">
+          <div class="spacing"></div>
+          <img src="images/placeholders/img_ph_2.png" class="img-responsive" alt="...">
+          <div class="news-date">
+            <p>20/02/2017</p>
+          </div>
+
+          <div class="caption">
+            <h3>Titulo da notícia</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea eum repellendus repudiandae sit nulla iusto earum fugiat ipsam eaque laborum non commodi, provident odit laboriosam animi necessitatibus incidunt consequuntur praesentium!</p>
+            <div class="spacing"></div>
+            <div class="spacing"></div>
+            <a href="#" class="btn-plus plus-yellow">
+              Leia mais
+            </a>
+            <div class="clearfix"></div>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4">
+        <div class="thumbnail">
+          <div class="spacing"></div>
+          <img src="images/placeholders/img_ph_2.png" class="img-responsive" alt="...">
+          <div class="news-date">
+            <p>20/02/2017</p>
+          </div>
+
+          <div class="caption">
+            <h3>Titulo da notícia</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea eum repellendus repudiandae sit nulla iusto earum fugiat ipsam eaque laborum non commodi, provident odit laboriosam animi necessitatibus incidunt consequuntur praesentium!</p>
+            <div class="spacing"></div>
+            <div class="spacing"></div>
+            <a href="#" class="btn-plus plus-blue">
+              Leia mais
+            </a>
+            <div class="clearfix"></div>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4">
+        <div class="thumbnail">
+          <div class="spacing"></div>
+          <img src="images/placeholders/img_ph_2.png" class="img-responsive" alt="...">
+          <div class="news-date">
+            <p>20/02/2017</p>
+          </div>
+
+          <div class="caption">
+            <h3>Titulo da notícia</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea eum repellendus repudiandae sit nulla iusto earum fugiat ipsam eaque laborum non commodi, provident odit laboriosam animi necessitatibus incidunt consequuntur praesentium!</p>
+            <div class="spacing"></div>
+            <div class="spacing"></div>
+            <a href="#" class="btn-plus plus-yellow">
+              Leia mais
+            </a>
+            <div class="clearfix"></div>
+          </div>
+        </div>
+      </div>
+
+    </div>
+
   </div>
 
 
