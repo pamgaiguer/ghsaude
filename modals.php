000b5ca20ff79e2c4ad6dd738387ef8f82f8e304
Pam Gaiguer <pamella.gaiguer@gmail.com>
2017-05-14 19:43:59 -0300

    partial for carousel

--- modals.php
+++ modals.php
@@ -2,72 +2,8 @@
 
 <main class="home">
 
-  <div id="slider-home" class="carousel slide" data-ride="carousel">
+<?php include 'includes/_carousel.php';?>
 
-    <div class="carousel-inner" role="listbox">
-      <div class="item active">
-        <img src="images/slider/slider_1.jpg">
-      </div>
-      <div class="item">
-        <img src="images/slider/slider_2.jpg">
-      </div>
-      <div class="item">
-        <img src="images/slider/slider_3.jpg">
-      </div>
-      <div class="item">
-        <img src="images/slider/slider_4.jpg">
-      </div>
-    </div>
-
-    <a class="left carousel-control" href="#slider-home" role="button" data-slide="prev">
-      <i class="material-icons">keyboard_arrow_left</i>
-    </a>
-    <a class="right carousel-control" href="#slider-home" role="button" data-slide="next">
-      <i class="material-icons">keyboard_arrow_right</i>
-    </a>
-  </div>
-  <div class="spacing"></div>
-  <div class="container">
-    <div class="row">
-      <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-        <div class="box-services hidden-xs">
-          <div class="box-blue">
-            <img src="images/rede_cred.png" class="img-responsive center-block" alt="">
-            <h2>Rede Credenciada</h2>
-            <p>
-              Aqui você poderá consultar toda a nossa Rede Credenciada e escolher
-              o melhor credenciado para cuidar de você e sua família.
-            </p>
-          </div>
-          <div class="box-yellow">
-            <img src="images/beneficiario-blue.png" class="img-responsive center-block" alt="">
-            <h2>Beneficiário</h2>
-            <p>
-              Acesso restrito para beneficiários do cartão GH Saúde.
-              Aqui você poderá consultar sua situação cadastral, agendamentos e outras informações.
-            </p>
-          </div>
-          <div class="box-blue">
-            <img src="images/casinha.png" class="img-responsive center-block" alt="">
-            <h2>Credenciado</h2>
-            <p>
-              Acesso restrito para os parceiros credenciados ao cartão GH Saúde.
-              Aqui você poderá efetuar consultas diversas, transmitir faturas, retirar guias e outros.
-            </p>
-          </div>
-          <div class="box-yellow">
-            <img src="images/pessoajuridica.png" class="img-responsive center-block" alt="">
-            <h2>Cliente Pessoa Jurídica</h2>
-            <p>
-              Sua empresa é nossa cliente? Acesse aqui seus relatórios, extratos, segunda vida de boletos,
-              recibos e outros.
-            </p>
-          </div>
-          <div class="clearfix"></div>
-        </div>
-      </div>
-    </div>
-  </div>
 
 
   <button type="button" class="btn btn-primary btn-lg" data-toggle="modal" data-target="#accessPages">
---
+++ news-open.php
@@ -0,0 +1,29 @@
+<?php include 'includes/_header.php';?>
+
+<main class="news-opens">
+<?php include 'includes/_carousel.php';?>
+
+
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+        <h3 class="title">
+          <img src="images/pgIcon_news.png" alt="">
+          Notícias
+        </h3>
+        <p>Confira abaixo as principais notícias da GH SAÚDE e também, da saúde no país.</p>
+      </div>
+    </div>
+
+    <div class="spacing"></div>
+    <div class="spacing"></div>
+    <div class="spacing"></div>
+
+
+
+  </div>
+
+
+</main>
+
+<?php include 'includes/_footer.php';?>
